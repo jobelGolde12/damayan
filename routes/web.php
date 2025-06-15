2163b3f5634858ec26456bc640cc6b1aed6c5733
jobelGolde12 <jobelgolde43@gmail.com>
2025-06-16 07:23:29 +0800

    Adding RegisteredMember page for the collector but the functionality is not finish

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\CollectorController;
 use App\Http\Controllers\ContributionController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\MemberControllerForCollector;
 use App\Http\Controllers\MembersController;
 use App\Http\Controllers\OfficialArchive;
 use App\Http\Controllers\OfficialController;
@@ -98,6 +99,7 @@ Route::prefix('role')->name('role.')->middleware('auth')->group(function () {
 //Collector
 Route::prefix('collector')->name('collector.')->middleware('auth')->group(function () {
     Route::get('/collector-dashboard', [CollectorController::class, 'index'])->name('collectorDashboard');
+    Route::get('view-members-as-collector', [MemberControllerForCollector::class, 'index'])->name('viewMembersAsCollector');
     // Route::get('/view-collector-contributions', [\App\Http\Controllers\CollectorController::class, 'viewContributions'])->name('viewContributions');
     // Route::get('/add-collector-contribution', [\App\Http\Controllers\CollectorController::class, 'addContribution'])->name('addContribution');
     // Route::post('/add-collector-contribution-post', [\App\Http\Controllers\CollectorController::class, 'storeContribution'])->name('storeContribution');
